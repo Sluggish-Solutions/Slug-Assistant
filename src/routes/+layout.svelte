fbaa8ac38965fa4ba78009305c275abc3c3a050e
suri312006 <suri312006@gmail.com>
2024-01-20 10:23:13 -0800

    working on header i think?

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import '../app.postcss';
+	import Header from "$lib/components/home/Header.svelte";
 </script>
-
+<Header />
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,7 @@
 </script>
 
 
+
 <div class="flex justify-between">
 	<div>
 		<SlugMommy/>
