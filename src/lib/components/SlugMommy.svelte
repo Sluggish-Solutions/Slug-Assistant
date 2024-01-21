e1b3b1aa3c318da9ac532d137d37c00e192e36ab
Kenric Tee <kenrictee@Kenrics-MacBook-Air.local>
2024-01-21 07:32:32 -0800

    asdf

--- src/lib/components/SlugMommy.svelte
+++ src/lib/components/SlugMommy.svelte
@@ -1,24 +1,23 @@
 <script lang="ts">
-    export let key: string;
-    let imgURL = [
-    "sprites/exercise.png",
-    "sprites/water.png",
-    "sprites/food.png",
-  ];
-    
+	// export let key: string
+	let imgURL = [
+		'sprites/exercise.png',
+		'sprites/water.png',
+		'sprites/food.png',
+	]
 
-  let i = 0;
-  let display1 = "";
-  let URL = "";
-  export function display(show: boolean, index: number) {
-    URL = imgURL[index];
-    display1 = show.toString();
-  }
+	let i = 0
+	let display1 = ''
+	let URL = ''
+	export function display(show: boolean, index: number) {
+		URL = imgURL[index]
+		display1 = show.toString()
+	}
 </script>
 
 <!-- <div>
   <img src={URL} alt="notworking" class="display:{display1}" />
 </div> -->
 <div class="flex w-full justify-center py-10">
-  <img src="sprites/food.png" alt="notworking"/>
+	<img src="sprites/food.png" alt="notworking" />
 </div>
