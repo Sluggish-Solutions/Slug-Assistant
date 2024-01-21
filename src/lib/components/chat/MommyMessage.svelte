7e42e3ca19d6764aaa8f28a16f96ed8784c4d178
suri312006 <suri312006@gmail.com>
2024-01-21 00:40:12 -0800

    I CAN TALK WITH MOMMYYYY!!!!!!!

--- src/lib/components/chat/MommyMessage.svelte
+++ src/lib/components/chat/MommyMessage.svelte
@@ -5,13 +5,23 @@
 </script>
 
 <div class="grid grid-cols-[auto_1fr] gap-2">
-	<Avatar src="https://i.pravatar.cc/?img={bubble.avatar}" width="w-12" />
+	<Avatar width="w-12" />
 	<div class="card p-4 variant-soft rounded-tl-none space-y-2">
 		<header class="flex justify-between items-center">
-			<p class="font-bold">{bubble.name}</p>
+			<p class="font-bold">Slug Mommy</p>
 			<small class="opacity-50">{bubble.timestamp}</small>
 		</header>
-		<p>{bubble.message}</p>
+		<p>{bubble.body}</p>
 	</div>
 </div>
+<!-- <div class="grid grid-cols-[auto_1fr] gap-2"> -->
+<!-- 	<Avatar src="https://i.pravatar.cc/?img={bubble.avatar}" width="w-12" /> -->
+<!-- 	<div class="card p-4 variant-soft rounded-tl-none space-y-2"> -->
+<!-- 		<header class="flex justify-between items-center"> -->
+<!-- 			<p class="font-bold">{bubble.name}</p> -->
+<!-- 			<small class="opacity-50">{bubble.timestamp}</small> -->
+<!-- 		</header> -->
+<!-- 		<p>{bubble.message}</p> -->
+<!-- 	</div> -->
+<!-- </div> -->
 					
--- src/routes/talk/+page.ts
+++ src/routes/talk/+page.ts
@@ -8,8 +8,6 @@ const sendMessage = async( curr_message: string) => {
 	const prev_messages = get_prev_messages()
 	//request my endpoint
 
-	console.log(curr_message)
-	console.log(prev_messages)
 	let user_id = get(curr_user_id)
 	let curr_convo_id = get(convo_id)
 
@@ -24,7 +22,6 @@ const sendMessage = async( curr_message: string) => {
 	})
 	const response = await res.json()
 
-	console.log("what the client gets back", response);
 	add_message_to_store(false, curr_message)
 	add_message_to_store(true, response.ai_response.content)
 	return response.ai_response.content
@@ -37,11 +34,19 @@ const { supabase, session } = await parent();
 	// needs to load data from supabase for the messages
 	// need to filter time stamp / library / something like that 
 	// need to load messages from the previous thing
- const curr_convo_id = get(convo_id)
+let curr_convo_id = get(convo_id)
 
 	if(curr_convo_id === ''){
+		console.log("creating conversation")
 		// make request to supabase to create a thingy
-		const convo_creation = await supabase.from("conversation").insert({user: session?.user.id}).select().single()
+		const convo_creation = await supabase
+			.from("conversation")
+		.insert({user: session?.user.id}).select().single()
+		
+		if (convo_creation.data) {
+			setConvoId(convo_creation.data.id.toString())
+			
+		}
 
 		if (convo_creation.error) {
 			return fail(404, {
@@ -52,11 +57,16 @@ const { supabase, session } = await parent();
 		setConvoId(convo_creation.data.id.toString())
 	}
 
-	const preMessagesQuery = await supabase.from('messages').select(`*, author(*)`).eq(`author`, curr_user_id)
-
+curr_convo_id = get(convo_id)
+	console.log('curr_convo_id', curr_convo_id)
+	const preMessagesQuery = await supabase.from('messages')
+		.select(`*, author(*)`)
+		.eq("author", session!.user.id )
+		.eq(`conversation_id`, parseInt(curr_convo_id))
+	console.log('query', preMessagesQuery)
 	let newMessageFeed = preMessagesQuery.data
 
-	console.log(newMessageFeed)
+	console.log("messages from db", newMessageFeed)
 	
 
 let messageFeed = [
@@ -80,7 +90,7 @@ let messageFeed = [
 		color: 'variant-soft-primary'
 	}
 ];
-	return { session, messageFeed , sendMessage}
+	return { session, messageFeed , sendMessage, newMessageFeed}
 }
 
 
