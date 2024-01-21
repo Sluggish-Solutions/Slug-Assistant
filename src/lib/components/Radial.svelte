76deefa7782e2f239a9a06768af9f376f5f9101a
Alb111 <albertfelix2005@gmail.com>
2024-01-20 22:46:01 -0800

    dasfdsf

--- src/lib/components/Radial.svelte
+++ src/lib/components/Radial.svelte
@@ -3,11 +3,12 @@
 	import { ProgressBar } from '@skeletonlabs/skeleton'
 	export let progress:number;
 	export let color:string;
+	export let title:string;
 </script>
 
 <div class="flex flex-col items-center w-full">
 		<div class="">
-			<h1 class="text-2xl my-4">Chores:</h1>
+			<h1 class="text-2xl my-4">{title}</h1>
 		</div>
 		<div class="z-[-10]">
 			<ProgressRadial meter={color} value={progress}
