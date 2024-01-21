7dffb8b3c8f5bdc09d541487d6defc8d26abc0af
suri312006 <suri312006@gmail.com>
2024-01-20 18:04:38 -0800

    finished client side of sending to mommy

--- src/routes/talk/+page.ts
+++ src/routes/talk/+page.ts
@@ -1,7 +1,30 @@
-import { prev_messages } from "../../stores/userStore";
+import { curr_user_id, prev_messages } from "../../stores/userStore";
 import type { PageLoad } from "./$types";
 import {get} from 'svelte/store'
 // }
+const sendMessage = async(convo_id:string, curr_message: string) => {
+	//get array of local storage messages
+	const messages_arr = get(prev_messages)
+	console.log(messages_arr)
+	//request my endpoint
+	//
+	let user_id = get(curr_user_id)
+
+	const res = await fetch('/api/send_message ', {
+		method: 'POST',
+		body: JSON.stringify({
+			"user_id": user_id,
+			"new_message": curr_message,
+			"message_history": messages_arr,
+		})
+	})
+	const response = await res.json()
+	
+	console.log(response);
+// ideally it would be nice if there was some sort of toast if some error happend?
+	//
+}
+	//save and return back whatever we get back
 export const load: PageLoad = async ({ parent }) => {
 const { session } = await parent();
 	// needs to load data from supabase for the messages
@@ -26,7 +49,7 @@ let messageFeed = [
 		color: 'variant-soft-primary'
 	}
 ];
-	return { session, messageFeed }
+	return { session, messageFeed , sendMessage}
 }
 
 
