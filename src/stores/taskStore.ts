9b94f3be2e082e9c98274f6681fbf5dee192b108
Alb111 <albertfelix2005@gmail.com>
2024-01-21 07:10:48 -0800

    data

--- src/stores/taskStore.ts
+++ src/stores/taskStore.ts
@@ -1,74 +1,174 @@
+
 import type { Writable } from 'svelte/store'
 import { writable } from 'svelte/store'
-// import { localStorageStore } from '@skeletonlabs/skeleton'
+import { localStorageStore } from '@skeletonlabs/skeleton'
+import { get } from 'svelte/store'
 export const curr_user_id = writable('')
 export const convo_id = writable('')
-export const first_time = writable(true)
-
+export const first_time = writable(true);
 export type Task = {
-	id: number
-	task_name: string
-	enabled: boolean
-	completed_today: boolean,
-	group: string,
-	last_updated: string
-	success: number
+	task_name: string,
+	enabled: boolean,
+	last_updated: string,
+	success: number,
 	occurences: number
 }
+export const tasks: Writable<Array<Task>> = localStorageStore(
+	'tasks',
+	[
+		{
+			task_name: 'Water Intake',
+			enabled: false,
+			last_updated: '',
+			success: 0,
+			occurences: 0,
+			completedToday:false,
+			group:"Health"
+		} as Task,
+		{
+			task_name: 'Food Intake',
+			enabled: false,
+			last_updated: '',
+			success: 0,
+			occurences: 0,
+			completedToday:false,
+			group:"Health"
+		} as Task,
+		{
+			task_name: 'Exersize',
+			enabled: false,
+			last_updated: '',
+			success: 0,
+			occurences: 0,
+			completedToday:false,
+			group:"Health"
+		} as Task,
+		{
+			task_name: 'Meditation',
+			enabled: false,
+			last_updated: '',
+			success: 0,
+			occurences: 0,
+			completedToday:false,
+			group:"Mindfulness"
+		} as Task,
+		{
+			task_name: 'Reflection',
+			enabled: false,
+			last_updated: '',
+			success: 0,
+			occurences: 0,
+			completedToday:false,
+			group:"Mindfulness"
 
-export const tasks: Writable<Task[]> = writable([
-	{
-		id: 1,
-		task_name: 'Brush Teeth',
-		enabled: false,
-		completed_today: false,
-		group: 'Health',
-		last_updated: `${Date.now()}`,
-		success: 0,
-		occurences: 0,
-	} as Task,
-	{
-		id: 2,
-		task_name: 'Wash Face',
-		enabled: false,
-		completed_today: false,
-		group: 'Health',
-		last_updated: `${Date.now()}`,
-		success: 0,
-		occurences: 0,
-	} as Task,
-])
+		} as Task,
+		{
+			task_name: 'Socailizing',
+			enabled: false,
+			last_updated: '',
+			success: 0,
+			occurences: 0,
+			completedToday:false,
+			group:"Mindfulness"
+		} as Task,
+		{
+			task_name: 'Letures',
+			enabled: false,
+			last_updated: '',
+			success: 0,
+			occurences: 0,
+			completedToday:false,
+			group:"Academics"
+		} as Task,
+		{
+			task_name: 'Study/Homework',
+			enabled: false,
+			last_updated: '',
+			success: 0,
+			occurences: 0,
+			completedToday:false,
+			group:"Academics"
+		} as Task,
 
-// updated to user's version on load if there exists
-export const toggle_task = (id: number) => {
-	tasks.update((tasks) => {
-		const updatedTasks = tasks.map((task) => {
-			if (task.id === id) {
-				return { ...task, enabled: !task.enabled }
-			}
-			return task
-		})
-		localStorage.setItem('tasks', JSON.stringify(updatedTasks))
-		return updatedTasks
-	})
+		{
+			task_name: 'Leetcode',
+			enabled: false,
+			last_updated: '',
+			success: 0,
+			occurences: 0,
+			completedToday:false,
+			group:"Academics"
+		} as Task,
+
+
+		{
+			task_name: 'Laundry',
+			enabled: false,
+			last_updated: '',
+			success: 0,
+			occurences: 0,
+			completedToday:false,
+			group:"Chores"
+		} as Task,
+
+		{
+			task_name: 'Cleaning',
+			enabled: false,
+			last_updated: '',
+			success: 0,
+			occurences: 0,
+			completedToday:false,
+			group:"Chores"
+		} as Task,
+
+		{
+			task_name: 'Trash Disposal',
+			enabled: false,
+			last_updated: '',
+			success: 0,
+			occurences: 0,
+			completedToday:false,
+			group:"Chores"
+		} as Task
+
+
+
+	]
+)
+
+export const add_task = (newTask: Task) => {
+	let old_tasks = [...get(tasks)]
+	old_tasks.unshift(newTask);
+	tasks.set(old_tasks);
 }
 
-export const toggle_completed = (id: number) => {
-	tasks.update((tasks) => {
-		const updatedTasks = tasks.map((task) => {
-			if (task.id === id) {
-				return { ...task, completed_today: !task.completed_today }
-			}
-			return task
-		})
-		localStorage.setItem('tasks', JSON.stringify(updatedTasks))
-		return updatedTasks
-	})
+export const edit_task = (idx: number, editedTask: Task) => {
+	let old_tasks = [...get(tasks)]
+
+	old_tasks.splice(idx, 1)
+
+	let new_arr = [
+		...old_tasks.slice(0, idx),
+		editedTask,
+		...old_tasks.slice(idx)
+	]
+
+
+
+	tasks.set(new_arr);
+
 }
 
-export const getDetails = (id: number) => {
-	// get associated categories
-	// basically a big detailed summary of users progress
+export const delete_task = (idx: number) => {
 
+	let old_tasks = [...get(tasks)]
+	if (idx > -1) { // only splice array when item is found
+		old_tasks.splice(idx, 1); // 2nd parameter means remove one item only
+	}
+	tasks.set(old_tasks);
+}
 
-} 
+export const getTasks = () => {
+	return get(tasks)
+}
+
