95783d02fed5f0ead23a7772a2e9aeff3f09dcbc
suri312006 <suri312006@gmail.com>
2024-01-20 23:23:19 -0800

    fixed thingy

--- src/lib/components/Radial.svelte
+++ src/lib/components/Radial.svelte
@@ -1,22 +1,21 @@
 <script lang="ts">
-	import { ProgressRadial } from '@skeletonlabs/skeleton'
-	import { ProgressBar } from '@skeletonlabs/skeleton'
-	export let progress: number
-	export let color: string
-	export let title:string;
+	import { ProgressRadial } from "@skeletonlabs/skeleton";
+	import { ProgressBar } from "@skeletonlabs/skeleton";
+	export let progress: number;
+	export let color: string;
+	export let title: string;
 </script>
 
 <div class="flex flex-col items-center w-full">
-		<div class="">
-			<h1 class="text-2xl my-4">{title}</h1>
-		</div>
-		<div class="z-[-10]">
-			<ProgressRadial meter={color} value={progress}
-				>{progress}%</ProgressRadial>
-		</div>
+	<div class="">
+		<h1 class="text-2xl my-4">{title}</h1>
 	</div>
 	<div class="z-[-10]">
-		<ProgressRadial meter={color} value={progress}>{progress}%</ProgressRadial
+		<ProgressRadial meter={color} value={progress}
+			>{progress}%</ProgressRadial
 		>
 	</div>
+</div>
+<div class="z-[-10]">
+	<ProgressRadial meter={color} value={progress}>{progress}%</ProgressRadial>
 </div>
