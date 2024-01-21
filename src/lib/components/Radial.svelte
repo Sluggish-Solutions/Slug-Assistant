678f8d68d28b387479543731a3aba665b4d3cd64
Alb111 <albertfelix2005@gmail.com>
2024-01-20 21:58:13 -0800

    made radical a component and added variables for connectavivty

--- src/lib/components/Radial.svelte
+++ src/lib/components/Radial.svelte
@@ -1,51 +1,17 @@
-<script>
+<script lang="ts">
 	import { ProgressRadial } from '@skeletonlabs/skeleton'
 	import { ProgressBar } from '@skeletonlabs/skeleton'
-	let progress = 50
+	export let progress:Number;
+	export let color:string;
 </script>
 
-<div class="flex flex-col mt-5 h-full">
-	<div class="flex mt-10 w-full justify-center">
-		<div class="mr-10">
-			<h1 class="mt-12">Health:</h1>
+<div class="flex flex-col items-center w-full">
+		<div class="">
+			<h1 class="text-2xl my-4">Chores:</h1>
 		</div>
 		<div class="z-[-10]">
-			<ProgressRadial meter="stroke-green-400" value={progress}
-				>{progress}%</ProgressRadial
-			>
+			<ProgressRadial meter={color} value={progress}
+				>{progress}%</ProgressRadial>
 		</div>
 	</div>
 
-	<div class="flex mt-10 w-full justify-center ">
-		<div class="mr-10 flex rounded-lg">
-			<h1 class="mt-12 text-1xl">Mindfullness:</h1>
-		</div>
-		<div class="z-[-10]">
-			<ProgressRadial meter="stroke-blue-400" value={progress}
-				>{progress}%</ProgressRadial
-			>
-		</div>
-	</div>
-
-	<div class="flex mt-10 w-full justify-center">
-		<div class="mr-10">
-			<h1 class="mt-12">Schooling:</h1>
-		</div>
-		<div class="z-[-10]">
-			<ProgressRadial meter="stroke-red-400" value={progress}
-				>{progress}%</ProgressRadial
-			>
-		</div>
-	</div>
-
-	<div class="flex mt-10 w-full justify-center">
-		<div class="mr-10">
-			<h1 class="mt-12">Chores:</h1>
-		</div>
-		<div class="z-[-10]">
-			<ProgressRadial meter="stroke-purple-400" value={progress}
-				>{progress}%</ProgressRadial
-			>
-		</div>
-	</div>
-</div>
--- src/lib/components/SlugMommy.svelte
+++ src/lib/components/SlugMommy.svelte
@@ -1,12 +1,10 @@
 <script>
-    let imgURL=[ "favicon.png", "favicon.png", "favicon.png", "favicon.png"];
-    let count=0;
+    let imgURL = ["favicon.png", "favicon.png", "favicon.png", "favicon.png"];
+    
 </script>
 
-
 <div>
-    {#each imgURL as URL}
-        <img src={URL} alt="not working" id={count.toString()}>
-        {count=count+1}
+    {#each imgURL as URL, count}
+        <img src={URL} alt="not working" id={count.toString()} class="hidden">
     {/each}
 </div>
